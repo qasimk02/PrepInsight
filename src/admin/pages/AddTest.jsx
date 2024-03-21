1c8ad0eb9c75c2fed64b7476c864413de88c3985
qasimk02 <qassy02@gmail.com>
2024-03-22 03:48:19 +0530

    Created Folder Structure

---
+++ src/admin/pages/AddTest.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+const AddTest = () => {
+  return (
+    <div>
+      Add Test Form
+    </div>
+  )
+};
+
+export default AddTest;
---
+++ src/user/pages/TestAnalytics.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+const TestAnalytics = (props) => {
+  return (
+    <div>
+      Test Analytics
+    </div>
+  )
+};
+
+export default TestAnalytics;
