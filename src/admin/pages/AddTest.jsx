6129c5e5502b05f9a20f8d6fcfe905988c9d71a4
qasimk02 <qassy02@gmail.com>
2024-03-22 03:51:55 +0530

    added input field details

--- src/admin/pages/AddTest.jsx
+++ src/admin/pages/AddTest.jsx
@@ -4,6 +4,48 @@ const AddTest = () => {
   return (
     <div>
       Add Test Form
+      
+      Collection: tests
+        - Document: [Test ID]
+            - testType
+            - total
+            - Score
+            - Rank
+            - Correct
+            - Incorrect
+            - Skipped
+            - Accuracy
+            - Completed
+            - Time Taken
+            - Percentile
+            - Collection: subjects
+            - Document: Physics
+                - total
+                - score
+                - correct
+                - incorrect
+                - skipped
+                - accuracy
+                - time taken
+                - Collection: chapters
+                - Document: [Chapter ID]
+                    - total
+                    - score
+                    - correct
+                    - incorrect
+                    - skipped
+                    - accuracy
+                    - time taken
+            - Collection: weakChapters
+            - Document: [Chapter ID]
+                - // Chapter details
+            - Collection: averageChapters
+            - Document: [Chapter ID]
+                - // Chapter details
+            - Collection: strongChapters
+            - Document: [Chapter ID]
+                - // Chapter details
+
     </div>
   )
 };
